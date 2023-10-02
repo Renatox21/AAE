58895d93fd0b64d60bde34509439d5f35872619f
Renatox21 <82481366+Renatox21@users.noreply.github.com>
2023-10-02 00:05:36 -0500

    Implementacion de Recursividad

--- ProyectoFinal-AAE/ProyectoFinal-AAE/Recursividad.Designer.cs
+++ ProyectoFinal-AAE/ProyectoFinal-AAE/Recursividad.Designer.cs
@@ -28,21 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNameLibro = new System.Windows.Forms.TextBox();
+            this.txtNroPagina = new System.Windows.Forms.TextBox();
+            this.listBoxSalida = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Total de Hojas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre del Libro:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nro.Paginas:";
+            // 
+            // txtNameLibro
+            // 
+            this.txtNameLibro.Location = new System.Drawing.Point(44, 73);
+            this.txtNameLibro.Name = "txtNameLibro";
+            this.txtNameLibro.Size = new System.Drawing.Size(156, 20);
+            this.txtNameLibro.TabIndex = 3;
+            // 
+            // txtNroPagina
+            // 
+            this.txtNroPagina.Location = new System.Drawing.Point(44, 125);
+            this.txtNroPagina.Name = "txtNroPagina";
+            this.txtNroPagina.Size = new System.Drawing.Size(156, 20);
+            this.txtNroPagina.TabIndex = 4;
+            // 
+            // listBoxSalida
+            // 
+            this.listBoxSalida.FormattingEnabled = true;
+            this.listBoxSalida.Location = new System.Drawing.Point(44, 160);
+            this.listBoxSalida.Name = "listBoxSalida";
+            this.listBoxSalida.Size = new System.Drawing.Size(156, 225);
+            this.listBoxSalida.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Recursividad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(245, 453);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxSalida);
+            this.Controls.Add(this.txtNroPagina);
+            this.Controls.Add(this.txtNameLibro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Recursividad";
             this.Text = "Recursividad";
             this.Activated += new System.EventHandler(this.Recursividad_Activated);
             this.Load += new System.EventHandler(this.Recursividad_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNameLibro;
+        private System.Windows.Forms.TextBox txtNroPagina;
+        private System.Windows.Forms.ListBox listBoxSalida;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- ProyectoFinal-AAE/ProyectoFinal-AAE/Recursividad.cs
+++ ProyectoFinal-AAE/ProyectoFinal-AAE/Recursividad.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static ProyectoFinal_AAE.Recursividad;
 
 namespace ProyectoFinal_AAE
 {
@@ -15,6 +16,12 @@ namespace ProyectoFinal_AAE
         public Recursividad()
         {
             InitializeComponent();
+        }
+
+        public class Libro
+        {
+            public String name { get; set; }
+            public int page { get; set; }
         }
 
         private void Recursividad_Load(object sender, EventArgs e)
@@ -26,6 +33,53 @@ namespace ProyectoFinal_AAE
         {
             Left = 0;
             Top = 0;
+        }
+
+        static int CalcularTotalPaginas(List<int> pagina, int indice)
+        {
+
+            if (indice == pagina.Count)
+                return 0;
+
+            int paginasRestantes = CalcularTotalPaginas(pagina, indice + 1);
+
+            return pagina[indice] + paginasRestantes;
+        }
+
+        List<Libro> lista = new List<Libro>();
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            listBoxSalida.Items.Clear();
+            List<int> num = new List<int>();
+            Libro libro = new Libro();
+            
+            libro.name = txtNameLibro.Text;
+            libro.page = int.Parse(txtNroPagina.Text);
+
+            lista.Add(libro);
+
+            foreach(var l in lista) 
+            {
+                num.Add(l.page);
+                listBoxSalida.Items.Add("El libro " + l.name + " tiene " + l.page + " paginas");
+            }
+
+            listBoxSalida.Items.Add("--------------------------------");
+            listBoxSalida.Items.Add("El total de paginas es:" + CalcularTotalPaginas(num, 0));
+            
+
+            txtNameLibro.Text = null;
+            txtNroPagina.Text = null;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtNameLibro.Text = null;
+            txtNroPagina.Text = null;
+            listBoxSalida.Items.Clear();
         }
     }
 }
